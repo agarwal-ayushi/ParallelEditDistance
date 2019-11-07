084d6b8f09c5b82d4d6dd8f1fa16e2f476857e7b
Ayushi Agarwal <ayushi.mnnit@gmail.com>
2019-11-07 15:30:53 +0530

    openmp code added v1

--- openmp.cpp
+++ openmp.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <math.h>
 #include <iostream>
+#include <omp.h>
 
 void printMatrix(int* matrix, int rows, int cols) {
   for (int i=0; i < rows; i++) {
@@ -28,9 +29,8 @@ int main(int argc, char* argv[]) {
   int* DP = new int [DP_rows*DP_cols];
   int i,j;
   initializeMatrix(DP, DP_rows, DP_cols);
-  struct timespec start_serial, end_serial;
-  double diff_serial;
-  clock_gettime(CLOCK_MONOTONIC_RAW, &start_serial);
+  double start, end, diff_parallel;
+  start = omp_get_wtime();
 
   for (i = 0; i < DP_rows; i++) {
     DP[i*DP_cols] = i;
@@ -38,19 +38,17 @@ int main(int argc, char* argv[]) {
   for (j = 0; j < DP_cols; j++) {
     DP[j] = j;
   }
+  #pragma omp parallel for schedule(static) private(i,j)
   for (i=1; i < DP_rows; i++) {
     for (j=1; j < DP_cols; j++) {
-      //int val = (X[j] != Y[i]) ? 2 : 0;
-      //std::cout << DP[(i-1)*DP_cols+j]+1 << "\t" << DP[i*DP_cols+(j-1)]+1 << "\t" << DP[(i-1)*DP_cols+(j-1)]+((X[i-1] != Y[j-1]) ? 1 : 0) << std::endl;
       DP[i*DP_cols+j] = std::min(std::min((DP[(i-1)*DP_cols+j]+1), (DP[i*DP_cols+(j-1)]+1)),(DP[(i-1)*DP_cols+(j-1)]+ ((X[i-1] != Y[j-1]) ? 1 : 0)));
     }
   }
-  clock_gettime(CLOCK_MONOTONIC_RAW, &end_serial);
-  diff_serial = double((end_serial.tv_sec - start_serial.tv_sec) + (end_serial.tv_nsec - start_serial.tv_nsec)/1000000000.0);
-
+  end = omp_get_wtime();
+	diff_parallel = end - start;
   std::cout << X << "\t" << X.length() << std::endl << Y << "\t" << Y.length() << std::endl;
   printMatrix(DP, DP_rows, DP_cols);
+  printf("Parallel Execution Time=%f\n",diff_parallel);
 
-  printf("Serial Execution Time=%f\n",diff_serial);
   return 0;
 }
